f8c742b75399ca44fac6d4a6dd99b87f6797f1f2
Landon Hodges <landonhodges@utexas.edu>
2025-05-21 11:05:40 -0500

    modified:   .eleventy.js 	modified:   _includes/essay.njk 	modified:   _site/breaths/If Language Be the Food of Thought, Write On/index.html     modified:   _site/breaths/index.html 	modified:   _site/css/styles.css 	modified:   _site/index.html

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,24 @@ module.exports = function(eleventyConfig) {
     return collectionApi.getFilteredByGlob("breaths/*.md");
   });
 
+  eleventyConfig.addFilter("readSupplementalFile", function(filePath) {
+    if (!filePath) return "";
+    
+    // Resolve the path - you may need to adjust this depending on your project structure
+    const fullPath = path.join(__dirname, filePath);
+    
+    try {
+      if (fs.existsSync(fullPath)) {
+        return fs.readFileSync(fullPath, 'utf8');
+      }
+      console.warn(`Supplemental file not found: ${fullPath}`);
+      return "";
+    } catch (err) {
+      console.error(`Error reading supplemental file ${fullPath}:`, err);
+      return "";
+    }
+  });
+
   return {
     dir: {
       input: ".",
